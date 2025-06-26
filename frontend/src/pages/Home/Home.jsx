ffd8fd5824989988a39636749765d687806f815e
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-06-26 16:26:37 +0300

    third commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+  const [category, setCategory] = useState("All")
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category} />
+    </div>
   )
 }
 
