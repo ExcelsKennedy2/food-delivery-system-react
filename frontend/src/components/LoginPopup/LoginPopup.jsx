70390983601a34471b9ce49f7619cdfccd78863d
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-07-01 09:16:51 +0300

    sixth commit

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
 
 const LoginPopup = () => {
+
+    const [currState, setCurrState] = useState("Sign Up")
   return (
     <div className='login-popup'>
-        login
+        <form action="" className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+            </div>
+        </form>
     </div>
   )
 }
