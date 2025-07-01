a9c4e896fa0643648a9e5386efbf08e9574509ac
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-07-01 16:53:50 +0300

    sixth commit

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -19,17 +19,21 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index)=>{
-          if(cartItems[item._id]>0)
-          {
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
             return (
-              <div className="cart-items-title cart-items-items">
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price*cartItems[item._id]}</p>
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>x</p>
+                </div>
+                <hr />
               </div>
+
             )
           }
         })}
