508e652198822d9b360388650e31fe239b79701c
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-07-15 14:25:37 +0300

    eighth commit - admin panel

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -18,10 +18,18 @@ const Add = () => {
         setData(data=>({...data,[name]:value}))
     }
 
+    const onSubmitHandler = async(event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('name', data.name)
+        formData.append('description', data.description)
+        formData.append('price', Number(data.price))
+        formData.append('category', data.category)
+    }
     
   return (
     <div className='add'>
-        <form className='flex-col'>
+        <form className='flex-col' onSubmit={onSubmitHandler}>
             <div className="add-img-upload flex-col">
                 <p>Upload Image</p>
                 <label htmlFor="image">
