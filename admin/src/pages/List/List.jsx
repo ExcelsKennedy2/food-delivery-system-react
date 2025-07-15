362c3dd867ea1a25db24b19493fbe0aa5b145a32
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-07-15 18:22:31 +0300

    eighth commit - admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './List.css'
 import axios from 'axios'
 import {toast} from 'react-toastify'
+
 const List = () => {
 
-  const url = 'http://localhost:4000'
+const url = 'http://localhost:4000'
 const [list, setList] = useState([]);
 
 const fetchList = async () => {
   const response = await axios.get(`${url}/api/food/list`);
+  console.log(response.data);
   if (response.data.success){
     setList(response.data.data);
   }
@@ -18,8 +20,34 @@ const fetchList = async () => {
   }
 }
 
+useEffect(()=>{
+  fetchList();
+},[])
+
   return (
-    <div></div>
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index)=>{
+          return (
+            <div key={index} className='list-table-format'>
+              <img src={`${url}/images/`+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p>X</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
